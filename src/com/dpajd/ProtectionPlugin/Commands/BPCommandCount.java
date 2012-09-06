a9c9f91784670484416fdfba5215dbcc2bbd009a
Stephen Smith <smithse87@gmail.com>
2012-09-06 05:57:52 -0700

    Handled potential NPE if player is offline/doesn't exist

--- src/com/dpajd/ProtectionPlugin/Commands/BPCommandCount.java
+++ src/com/dpajd/ProtectionPlugin/Commands/BPCommandCount.java
@@ -1,11 +1,13 @@
 package com.dpajd.ProtectionPlugin.Commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.dpajd.ProtectionPlugin.Main.Main;
+import com.dpajd.ProtectionPlugin.Main.Main.MsgType;
 import com.dpajd.ProtectionPlugin.Regions.Owner;
 
 public class BPCommandCount extends BPCommand{
@@ -20,7 +22,16 @@ public class BPCommandCount extends BPCommand{
 		if (player != null){
 			if (args.length > 0){
 				Player target = Bukkit.getPlayer(args[0]);
-				plugin.sendMessage(player, target.getName() + " has a total of " + new Owner(target).getRegionCount() + " region protections.");
+				if (target == null){
+					OfflinePlayer targetOffline = Bukkit.getOfflinePlayer(args[0]);
+					if (targetOffline != null){
+						plugin.sendMessage(player, targetOffline.getName() + " has a total of " + new Owner(targetOffline.getName()).getRegionCount() + " region protections.");
+					}else{
+						plugin.sendMessage(player, MsgType.ERROR, "Player doesn't exist.");
+					}
+				}else{
+					plugin.sendMessage(player, target.getName() + " has a total of " + new Owner(target).getRegionCount() + " region protections.");
+				}
 			}else{
 				plugin.sendMessage(player, player.getName() + " has a total of " + new Owner(player).getRegionCount() + " region protections.");
 			}
