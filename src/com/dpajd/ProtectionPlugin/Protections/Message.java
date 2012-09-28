41bc0cd1302aabc56eaa1a006eec22594679adab
Stephen Smith <smithse87@gmail.com>
2012-09-28 15:20:15 -0700

    ...

--- src/com/dpajd/ProtectionPlugin/Protections/Message.java
+++
@@ -1,9 +0,0 @@
-package com.dpajd.ProtectionPlugin.Protections;
-
-import com.dpajd.ProtectionPlugin.Regions.RegionMessages.RegionMessageType;
-
-public interface Message {
-
-	public abstract RegionMessageType getMessageType();
-	
-}
